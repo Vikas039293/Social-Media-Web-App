110c17dd875ed6dbe26be2c3dbcd040c4ef8a8dd
Vikas Choudhary <vikaschoudhary122001@gmail.com>
2023-11-06 19:13:14 +0530

    Post Url Update

--- server/Routes/compose.js
+++ server/Routes/compose.js
@@ -1,6 +1,9 @@
 const Profile= require("../DataBase/UserData");
 const cloudinary = require('cloudinary').v2;
 require("dotenv").config();
+const shortid = require('shortid');
+const uniqueId = shortid.generate();
+
 cloudinary.config({ 
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
     api_key: process.env.CLOUDINARY_API_KEY, 
@@ -8,12 +11,13 @@ cloudinary.config({
   });
 const compose=async (req,res)=>{
     const {username,file,fileType,caption}=req.body.data;
+    const imageId=username+"_"+uniqueId;
     const fileUrl=fileType==='image'?(
             await cloudinary.uploader.upload(file,
-            {public_id: username})
+            {public_id: imageId})
         ):(
             await cloudinary.uploader.upload(file,
-                {resource_type:fileType, public_id: username})
+            {resource_type:fileType, public_id: imageId})
         );
 
     const update={
