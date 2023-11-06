12366612f54df47077ddb72209edf74e92cb62f3
Vikas Choudhary <vikaschoudhary122001@gmail.com>
2023-11-06 18:28:26 +0530

    Upload url Update

--- server/utils/uploadImg.js
+++ server/utils/uploadImg.js
@@ -3,6 +3,8 @@ const jwt = require('jsonwebtoken');
 require("dotenv").config();
 const secret = process.env.JWT_SECRET;
 const cloudinary = require('cloudinary').v2;
+const shortid = require('shortid');
+const uniqueId = shortid.generate();
 cloudinary.config({ 
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
     api_key: process.env.CLOUDINARY_API_KEY, 
@@ -14,10 +16,9 @@ const uploadImg=async (req,res)=>{
         token=req.header('Authorization').split(' ')[1];
         const decoded = jwt.verify(token, secret);
         const base64Image=req.body.image;
-        const image_id=decoded.username;
+        const image_id=decoded.username+uniqueId;
         const imgUrl=await cloudinary.uploader.upload(base64Image,
             { public_id: image_id},(err)=> {console.log(err)});
-
         //  console.log(imgUrl.secure_url);
         const update= await Profile.findOneAndUpdate({username:decoded.username},{profilePic:imgUrl.secure_url});
 
